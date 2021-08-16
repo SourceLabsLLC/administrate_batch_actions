e0dd2a0954a76f90e04551cd3407d74b1513fa1f
Andrei Bondarev <andrei.bondarev13@gmail.com>
2021-08-15 22:02:41 -0400

    Init

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+AdministrateBatchActions::Engine.routes.draw do
+
+  def controllers_for(path)
+    path ||= Rails.root.join('app', 'controllers', 'admin', '**/*.rb')
+
+    Dir[path].each do |file|
+      require_dependency file
+    end
+
+    ::Administrate::ApplicationController.descendants
+  end
+
+  controllers_for(nil).each do |controller|
+    controller
+      .instance_methods
+      .select { |m| m[/.+_batch_action$/] }
+      .each do |method_name|
+        resource = controller.name.demodulize.underscore.delete_suffix('_controller')
+
+        resources resource.to_sym, only: :none do
+          get method_name.to_sym, on: :collection
+        end
+    end
+  end
+
+end
--- lib/administrate_batch_actions.rb
+++ lib/administrate_batch_actions.rb
@@ -4,11 +4,18 @@ require 'active_support/core_ext'
 require 'active_support/dependencies'
 require 'administrate/engine'
 
-require 'administrate_batch_actions/router'
+# require 'administrate_batch_actions/router'
+require 'administrate_batch_actions/helpers'
 require 'administrate_batch_actions/version'
 
 module AdministrateBatchActions
   class Engine < ::Rails::Engine
     Administrate::Engine.add_javascript 'administrate_batch_actions/script'
+
+    initializer "administrate_batch_actions.engine" do |app|
+      ActionView::Base.send :include, AdministrateBatchActions::Helpers
+      # binding.pry
+      # AdministrateBatchActions::Router.new(Rails.application.routes.router).call
+    end
   end
 end
