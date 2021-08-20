35d90ef247ac05aa8b83a7d211e60408495bd762
Andrei Bondarev <andrei.bondarev13@gmail.com>
2021-08-19 21:47:56 -0400

    Fix specs

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -1,4 +1,4 @@
-require './rails_helper'
+require 'rails_helper'
 
 RSpec.describe 'Helpers', type: :helper do
   it 'renders action button' do
--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -1,4 +1,4 @@
-require './rails_helper'
+require 'rails_helper'
 
 RSpec.describe 'Routes', type: :routing do
   it 'has route to the batch action' do
