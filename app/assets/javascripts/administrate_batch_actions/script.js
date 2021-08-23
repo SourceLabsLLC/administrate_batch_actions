37fbc17f1edfc413a558e0371e5f71e01d58b8da
Andy Wang <wangyaodi@gmail.com>
2021-08-23 19:34:55 +0800

    update js to toggle action buttons based on selections

--- app/assets/javascripts/administrate_batch_actions/script.js
+++ app/assets/javascripts/administrate_batch_actions/script.js
@@ -2,22 +2,18 @@ var buttons = document.querySelectorAll("[data-batch-action-option='button']");
 var checkboxes = document.querySelectorAll("[data-batch-action-option='checkbox']");
 var selectAllCheckboxes = document.querySelector("[data-batch-action-option='select_all']");
 
-if (selectAllCheckboxes) {
+if (selectAllCheckboxes && checkboxes && buttons) {
   selectAllCheckboxes.addEventListener('click', function(){
     checkboxes.forEach(function(checkbox) {
       checkbox.checked = selectAllCheckboxes.checked;
     });
+
+    checkAndToggleActionButtons();
   });
 
   buttons.forEach(function(button){
     button.addEventListener('click', function(event){
-      var ids = Array.prototype.filter.call(checkboxes, function(checkbox) {
-        if (checkbox.checked) { return checkbox }
-      }).map(function(checkbox) {
-        return 'batch_action_ids[]=' + checkbox.value
-      }).join('&');
-
-      button.href += '?' + ids
+      button.href += '?' + selectedItemIds()
     });
   });
 
@@ -28,6 +24,29 @@ if (selectAllCheckboxes) {
 
     checkbox.addEventListener('click', function(event) {
       event.stopImmediatePropagation();
+
+      checkAndToggleActionButtons();
     })
   })
+}
+
+function selectedItemIds() {
+  var ids = Array.prototype.filter.call(checkboxes, function(checkbox) {
+        if (checkbox.checked) { return checkbox }
+      }).map(function(checkbox) {
+        return 'batch_action_ids[]=' + checkbox.value
+      }).join('&');
+  return ids;
+}
+
+function checkAndToggleActionButtons() {
+  if (selectedItemIds()) {
+    buttons.forEach(function(button){
+      button.classList.remove('disabled');
+    });
+  } else {
+    buttons.forEach(function(button){
+      button.classList.add('disabled');
+    });
+  }
 }
