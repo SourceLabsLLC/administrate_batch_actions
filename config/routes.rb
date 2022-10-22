092e678a34abf1d1fb2d1772ed36fdd6f32fa7bb
Andrei Bondarev <andrei.bondarev13@gmail.com>
2022-10-22 17:38:14 -0500

    bump version

--- config/routes.rb
+++ config/routes.rb
@@ -10,19 +10,18 @@ Rails.application.routes.draw do
     ::Administrate::ApplicationController.descendants
   end
 
-  namespace :admin do
-    controllers_for(nil).each do |controller|
-      controller
-        .instance_methods
-        .select { |m| m[/.+_batch_action$/] }
-        .each do |method_name|
-          resource = controller.name.demodulize.underscore.delete_suffix('_controller')
+  controllers_for(nil).each do |controller|
+    controller
+      .instance_methods
+      .select { |m| m[/.+_batch_action$/] }
+      .each do |method_name|
+        resource = controller.name.demodulize.underscore.delete_suffix('_controller')
 
-          resources resource.to_sym do
-            post method_name.to_sym, on: :collection
-          end
-      end
+        resources resource.to_sym do
+          post method_name.to_sym,
+            on: :collection,
+            controller: controller.name.underscore.delete_suffix('_controller')
+        end
     end
   end
-
 end
