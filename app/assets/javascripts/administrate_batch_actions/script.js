3dcad2116d94bfa09ae6b441bc465ba6a16a2d01
Josh Frankel <joshmfrankel@gmail.com>
2026-04-02 15:34:04 -0400

    Ensure disabled state works as expected by changing links to buttons
    * Use WeakSet to avoid attaching multiple event handlers to the same element from multiple init() calls
    * Append hidden input within the button_to form to contain all selected batch_action_ids
    * Set Button disabled as default state
    * Ensure data-confirm is pulled into confirmation dialog (Rails 7+ doesn't confirm with UJS)

--- app/assets/javascripts/administrate_batch_actions/script.js
+++ app/assets/javascripts/administrate_batch_actions/script.js
@@ -1,13 +1,25 @@
-const init = function() {
-  var buttons = document.querySelectorAll("[data-batch-action-option='button']");
-  var checkboxes = document.querySelectorAll("[data-batch-action-option='checkbox']");
-  var selectAllCheckboxes = document.querySelector("[data-batch-action-option='select_all']");
+const boundForms = new WeakSet();
+const boundCheckboxes = new WeakSet();
+const boundSelectAllCheckboxes = new WeakSet();
+
+const init = function () {
+  var buttons = document.querySelectorAll(
+    "[data-batch-action-option='button']",
+  );
+  const forms = document.querySelectorAll(
+    "form:has([data-batch-action-option='button'])",
+  );
+  var checkboxes = document.querySelectorAll(
+    "[data-batch-action-option='checkbox']",
+  );
+  var selectAllCheckboxes = document.querySelector(
+    "[data-batch-action-option='select_all']",
+  );
 
   if (selectAllCheckboxes && checkboxes && buttons) {
-
-    window.onpageshow = function(event) {
-      if (selectedItemIds()) {
-        checkboxes.forEach(function(checkbox) {
+    window.onpageshow = function (event) {
+      if (selectedItemIds() && selectedItemIds().length === 0) {
+        checkboxes.forEach(function (checkbox) {
           checkbox.checked = false;
         });
 
@@ -15,57 +27,98 @@ const init = function() {
       }
     };
 
-    selectAllCheckboxes.addEventListener('click', function(){
-      checkboxes.forEach(function(checkbox) {
-        checkbox.checked = selectAllCheckboxes.checked;
-      });
+    if (!isBoundElement(boundSelectAllCheckboxes, selectAllCheckboxes)) {
+      boundSelectAllCheckboxes.add(selectAllCheckboxes);
 
-      checkAndToggleActionButtons();
+      selectAllCheckboxes.addEventListener("click", function () {
+        checkboxes.forEach(function (checkbox) {
+          checkbox.checked = selectAllCheckboxes.checked;
+        });
+
+        checkAndToggleActionButtons();
+      });
+    }
+
+    forms.forEach(function (form) {
+      if (isBoundElement(boundForms, form)) return;
+      boundForms.add(form);
+
+      const confirmMessage =
+        form.querySelector("[data-confirm]")?.dataset?.confirm ||
+        "Are you sure you want to submit this form?";
+
+      form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        if (confirm(confirmMessage) === true) {
+          selectedItemIds().forEach(function (id) {
+            const hiddenInput = document.createElement("input");
+            hiddenInput.type = "hidden";
+            hiddenInput.name = "batch_action_ids[]";
+            hiddenInput.value = id;
+            form.appendChild(hiddenInput);
+          });
+          form.submit();
+        } else {
+          return;
+        }
+      });
     });
 
-    buttons.forEach(function(button){
-      button.addEventListener('click', function(event){
-        button.href += '?' + selectedItemIds()
-      });
-    });
+    checkboxes.forEach(function (checkbox) {
+      if (isBoundElement(boundCheckboxes, checkbox)) {
+        return;
+      }
+      boundCheckboxes.add(checkbox);
 
-    checkboxes.forEach(function(checkbox){
-      checkbox.closest('td').addEventListener('click', function(event){
+      checkbox.closest("td").addEventListener("click", function (event) {
         event.stopImmediatePropagation();
-      })
+      });
 
-      checkbox.addEventListener('click', function(event) {
+      checkbox.addEventListener("click", function (event) {
         event.stopImmediatePropagation();
 
         checkAndToggleActionButtons();
-      })
-    })
+      });
+    });
   }
 
   function selectedItemIds() {
-    var ids = Array.prototype.filter.call(checkboxes, function(checkbox) {
-          if (checkbox.checked) { return checkbox }
-        }).map(function(checkbox) {
-          return 'batch_action_ids[]=' + checkbox.value
-        }).join('&');
+    var ids = Array.prototype.filter
+      .call(checkboxes, function (checkbox) {
+        if (checkbox.checked) {
+          return checkbox;
+        }
+      })
+      .map(function (checkbox) {
+        return checkbox.value;
+      });
     return ids;
   }
 
   function checkAndToggleActionButtons() {
-    if (selectedItemIds()) {
-      buttons.forEach(function(button){
-        button.classList.remove('disabled');
-        button.removeAttribute('disabled');
+    if (selectedItemIds() && selectedItemIds().length > 0) {
+      buttons.forEach(function (button) {
+        button.disabled = false;
       });
     } else {
-      buttons.forEach(function(button){
-        button.classList.add('disabled');
-        button.setAttribute('disabled', 'disabled');
+      buttons.forEach(function (button) {
+        button.disabled = true;
       });
     }
   }
+
+  function isBoundElement(boundElement, incomingElement) {
+    return boundElement.has(incomingElement);
+  }
 };
 
-document.addEventListener("DOMContentLoaded", function() { init() });
-document.addEventListener("turbolinks:load", function() { init() });
-document.addEventListener("turbo:load", function() { init() });
+document.addEventListener("DOMContentLoaded", function () {
+  init();
+});
+document.addEventListener("turbolinks:load", function () {
+  init();
+});
+document.addEventListener("turbo:load", function () {
+  init();
+});
